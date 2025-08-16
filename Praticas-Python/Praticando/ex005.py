0e6c96f35e2d60382205e60b831ac4bf1fb013eb
Jhon Jackson S S <radicalug@gmail.com>
2025-08-16 12:04:40 -0300

    02

---
+++ Praticas-Python/Praticando/ex005.py
@@ -0,0 +1,14 @@
+# a = 1
+# b = 1
+# for i in range(1, 10):
+#     c = a + b
+#     a = b
+#     b = c
+#     if c % 2 == 0:
+#         print(c, end=" ")
+
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print(j, end="")
+
+    print()

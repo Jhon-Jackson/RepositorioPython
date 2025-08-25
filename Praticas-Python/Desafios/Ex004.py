43296aa5118a56fda6d8f1fc268ffe9bf9c4db67
Jhon Jackson S S <radicalug@gmail.com>
2025-08-25 11:54:00 -0300

    05

---
+++ Praticas-Python/Desafios/Ex004.py
@@ -0,0 +1,4 @@
+#4.  Escreva um programa que a partir da idade de uma pessoa expressa em anos,
+# meses e dias, apresente a idade apenas em dias (considerar o ano com 365 e cada
+# mês com 30 dias).
+
